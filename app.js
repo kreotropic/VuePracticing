597d0ba4a8d404a6acadd53f07b1d796b45859b5
Ricardo Ferreira <rsfneg@gmail.com>
2023-10-15 12:46:09 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: "The Final Empire",
+            author: "Brandon Sanderson",
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+             console.log("changeTitle")
+             this.title = title
+        },
+        // changeAge(age) {
+        //     this.age = age
+        // }
+    },
+
+})
+app.mount('#app')
+
+
+v
